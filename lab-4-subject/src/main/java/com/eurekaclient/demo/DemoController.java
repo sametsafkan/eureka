57d5b7cc0a2a4a18be20d39b98806a5fbc375abf
msametsafkan <msametsafkan@192.168.1.21>
2017-09-02 22:53:19 +0300

    spring cloud bus entegrasyonu yapıldu

--- lab-4-subject/src/main/java/com/eurekaclient/demo/DemoController.java
+++ lab-4-subject/src/main/java/com/eurekaclient/demo/DemoController.java
@@ -1,12 +1,14 @@
 package com.eurekaclient.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DemoController {
 
 	@Value("${words}")
